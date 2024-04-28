b713bb5e022ef007cc6a4d9743910104eaae7644
unknown <debanilroy@gmail.com>
2024-04-28 11:09:29 +0530

    Added Required Function Declarations

--- q-one.cpp
+++ q-one.cpp
@@ -1,3 +1,20 @@
-class gaussian {
-    int a = 0;
-};
+#include <iostream>
+using namespace std;
+
+#define N 3
+
+void row_swap (int equat [N][N+1], int k, int j);
+
+void triangular (int equat [N][N+1]);
+
+void back_sub (int equat [N][N+1]);
+
+void print_equations (int equat [N][N+1]);
+
+int equations [N][N+1] =    {{2, 1, 1, 7},
+                            {3, 2, 3, 16},
+                            {4, 5, 3, 23}};
+
+int main() {
+    printf("OK");
+}
