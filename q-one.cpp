5799d5993905b9b474d02c6963650033b5c581fc
unknown <debanilroy@gmail.com>
2024-04-28 12:21:41 +0530

    Added Definitions
    Added function definitions for print_equations and row_swap

--- q-one.cpp
+++ q-one.cpp
@@ -3,18 +3,35 @@ using namespace std;
 
 #define N 3
 
-void row_swap (int equat [N][N+1], int k, int j);
+void print_equations (int equat [N][N+1]) {
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N + 1; j++) {
+            printf("%d ", equat[i][j]);
+        }
+        printf("\n");
+    }
+};
+
+void row_swap (int equat [N][N+1], int i, int j) {
+    int k;
+    i = i - 1, j = j - 1;
+    for (k = 0; k < N + 1; k++) {
+        int temp = equat [i][k];
+        equat [i][k] = equat [j][k];
+        equat [j][k] = temp;
+    }
+};
 
 void triangular (int equat [N][N+1]);
 
 void back_sub (int equat [N][N+1]);
-
-void print_equations (int equat [N][N+1]);
 
 int equations [N][N+1] =    {{2, 1, 1, 7},
                             {3, 2, 3, 16},
                             {4, 5, 3, 23}};
 
 int main() {
-    printf("OK");
+    print_equations(equations);
+    row_swap(equations, 1, 3);
 }
