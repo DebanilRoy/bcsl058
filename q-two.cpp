f83e050dd0fcf7230d0ff614c8a7d331cc5ae6b6
unknown <debanilroy@gmail.com>
2024-05-04 19:20:33 +0530

    Final Version

--- q-two.cpp
+++ q-two.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <thread>
+#include <unistd.h>
 using namespace std;
 
 #define N 4
@@ -10,11 +12,13 @@ void bisection(float equat[N], float range[2]) {
     while (true) {
         float x = (range[0] + range[1]) / 2;
         for (i = 0; i < N; i++) {
-            value = value + equat[i] * pow(x, (N - 1) - i);
+            value = value + equat[i] * pow(x, (N - 1) - i); 
         }
-        if (value == 0 && (value < 0.1 || value > -0.1)) {
-            cout<<"x = "<<x;
-            break;
+        cout<<"For x = "<<x<<" f(x) = "<<value<<endl;
+        usleep(500000);
+        if (value == 0 || (value > -0.01 && value < 0.01)) {
+            cout<<"x = "<<x<<endl;
+            return;
         }
         else if (value < 0) {
             float newRange[2] = {x, range[1]};
@@ -24,12 +28,13 @@ void bisection(float equat[N], float range[2]) {
             float newRange[2] = {range[0], x};
             bisection(equat, newRange);
         }
+        return;
     }
 }
 
 int main() {
     float equation[N] = {1, 0, -5, 1};
-    float range[2] = {1,2};
+    float range[2] = {1,3};
     bisection(equation, range);
     return 0;
 }
